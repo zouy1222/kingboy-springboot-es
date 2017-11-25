87a0db9da5bc156b26eec0c927b217a3a5bf3350
KingBoy <kingboyworld@163.com>
2017-11-25 19:36:48 +0800

    init

---
+++ src/main/java/com/kingboy/KingboySpringbootEsApplication.java
@@ -0,0 +1,12 @@
+package com.kingboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingboySpringbootEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingboySpringbootEsApplication.class, args);
+	}
+}
